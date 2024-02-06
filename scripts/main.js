8d98a402c5e6fbffc1f5e7547ef89c11bbda93df
kevinlockett <chlockett@msn.com>
2024-02-06 14:25:22 -0600

    added image to title

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,12 @@ import { Assignments } from "./Assignments.js"
 const mainContainer = document.querySelector("#container")
 
 const applicationHTML = `
-<h1>DeShawns Dog Walking</h1>
+<div class="title">
+    <section class="title__container">
+        <img src="./img/dog.png" alt="DeShawn's beagle, Skip" />
+        <h1>DeShawns Dog Walking</h1>
+    </section>
+</div>
 <article class="details">
     <section class="detail--column list details__cities">
         <h2>Cities with Service</h2>
